2ba42551ff2998550b39e96ac376247f81b069e5
brianq-nisra <brian.quinn@nisra.gov.uk>
2024-08-05 17:43:41 +0100

    renv stuff and banner function

--- code/config.R
+++ code/config.R
@@ -3,4 +3,24 @@ library(knitr)
 library(shiny)
 library(dplyr)
 library(kableExtra)
-source("functions/functions.R")
+library(httpuv)
+
+nics_theme <- "dof"
+
+source(paste0(here(), "/code/Functions/functions.R"))
+
+nisra_logo <- encodeURIComponent(paste0(here(), "/Data/Bilingual white logo.svg"))
+
+nisra_alt <- "NISRA logo"
+
+# Departmental logo
+dep_logo <- base64enc::dataURI(file = paste0(here(), "/Data/dept_logos/logo-white-",
+  nics_theme, ".png"
+))
+
+dep_alt <- paste(toupper(nics_theme), "logo")
+
+departmental_link <- "https://www.finance-ni.gov.uk/"
+
+title <- "Guidance on the collection and reporting of section 75 categories"
+
